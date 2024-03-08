f12caae9d0d4ed40042d65c7196f200d91fd3e95
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-08 09:25:23 +0000

    fastboot = 1.0

--- device.mk
+++ device.mk
@@ -44,7 +44,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
     # Display
