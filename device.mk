ce92f836b3203efab75aebf5ff438d410f010872
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-18 18:13:09 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -34,9 +34,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service \
     bootctrl.$(PRODUCT_PLATFORM) \
     bootctrl.$(PRODUCT_PLATFORM).recovery \
 
