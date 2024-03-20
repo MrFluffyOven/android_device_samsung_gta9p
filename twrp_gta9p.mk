99cd5cd6a4746222cd06a4c2e790ef5f83be8c5e
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-20 10:05:45 +0000

    Update twrp_gta9p.mk

--- twrp_gta9p.mk
+++ twrp_gta9p.mk
@@ -6,7 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 #$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
@@ -16,7 +16,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from gta9p device
 $(call inherit-product, device/samsung/gta9p/device.mk)
-$(call inherit-product, device/qcom/qssi/device.mk)
 
 PRODUCT_DEVICE := gta9p
 PRODUCT_NAME := twrp_gta9p
