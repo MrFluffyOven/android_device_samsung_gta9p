7fe1ba2d537848b32d301706c92ca46e1d25f9fe
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-28 10:13:20 +0000

    restart from scratch (test commit)

--- device.mk
+++ device.mk
@@ -6,39 +6,21 @@
 #
 LOCAL_PATH := device/samsung/gta9p
 
-# Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 32
-
 # API levels
-BOARD_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 32
 
 # define hardware platform
 PRODUCT_PLATFORM := holi
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_verifier \
-    update_engine_sideload \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl-wrapper.recovery \
-    android.hardware.boot@1.0-impl-wrapper \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.$(PRODUCT_PLATFORM) \
-    bootctrl.$(PRODUCT_PLATFORM).recovery \
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    fastbootd   
 
-    # Display
+# Recovery modules/dependencies
 TARGET_RECOVERY_DEVICE_MODULES += \
+    libdisplayconfig.qti \
     libion \
     libxml2 \
     libdmabufheap \
@@ -47,10 +29,20 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2 \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
 # QCOM
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+    
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
