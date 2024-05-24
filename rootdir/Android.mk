beab2332648ebd3328bd242b73c0322e90c66f4a
nokidevz <petreskinove1@gmail.com>
2024-05-24 20:19:08 +0200

    gta9p: Add root directory

---
+++ rootdir/Android.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
